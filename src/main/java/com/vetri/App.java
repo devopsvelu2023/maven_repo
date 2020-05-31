0506e96d0c904d70405d703548e4db9659a13fab
rajagvel <gopal.63murugan@gmail.com>
2020-05-31 13:48:49 +0000

    initial commit

---
+++ src/main/java/com/vetri/App.java
@@ -0,0 +1,13 @@
+package com.vetri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
